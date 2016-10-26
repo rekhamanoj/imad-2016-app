48457e88b4824eef503ceff0c6f9519ce593b930
rekhamanoj <rekhamanoj@users.noreply.github.com>
2016-10-25 23:49:04 -0700

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -8,6 +8,14 @@ app.use(morgan('combined'));
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
+app.get('/article-one',function(req,res)
+{res.sendfile("Article-one requested and will be served here")});
+
+app.get('/article-two',function(req,res)
+{res.sendfile("Article-two requested and will be served here")});
+
+app.get('/article-three',function(req,res)
+{res.sendfile("Articl-three requested and will be served here")});
 
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
