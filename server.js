24e6abed2105f8bab2fbb49aefbaeb46518ad66e
rekhamanoj <rekhamanoj@users.noreply.github.com>
2016-11-02 00:31:20 -0700

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -21,16 +21,55 @@ var content = {
              
              </p>`
     
+};
+function createTemplate(data) {
+     var title=data.title;
+     var heading=data.heading;
+     var date=data.date;
+     var content=data.content;
+     var htmlTemplate = `<html>
+    <head>
+        <title>
+           ${title}
+        </title>
+        <meta name = "viewport" content= "width = device-width , initial-scale=1"/>
+    
+    <link href="/ui/style.css" rel="stylesheet" />
+
+   
+    
+    </head>
+    
+    <body>
+        <div class="container" >                         
+           <div>
+              <a href = '/' > Home </a>
+           </div>
+           <hr/>
+           <h3>
+             ${heading}
+           </h3>
+           <div>
+            ${date}
+           </div>
+          ${content}
+       
+           </div>
+        
+        </div>
+    </body>
+</html>
+
+`;
+return htmlTemplate;
+    
 }
-
-
-
 
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 app.get('/article-one',function(req,res)
-{res.sendFile(path.join(__dirname, 'ui', 'article-one.html'));    });
+{res.send(createTempate(article-one));    });
 
 app.get('/article-two',function(req,res)
 {res.sendFile(path.join(__dirname, 'ui', 'article-two.html'));    });
