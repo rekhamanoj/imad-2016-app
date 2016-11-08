7343d7005a8ec36b3fd7860cc906effb4d39c5b8
rekhamanoj <rekhamanoj@users.noreply.github.com>
2016-11-08 08:21:00 +0000

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ var path = require('path');
 var app = express();
 app.use(morgan('combined'));
 
-var articles ={
+var articles = {
    var articleOne: {
     title : 'Article One',
     heading: 'Article one',
